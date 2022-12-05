ecb12db6d5e202066f056bfd397d13c83d4a12d0
matthew <m1glassman@student.bridgew.edu>
2022-12-04 21:00:53 -0500

    created class for an individual card.

--- cards.py
+++ cards.py
@@ -14,3 +14,17 @@ class Suite:
     def __init__(self, suite):
         self.suite = suite
 
+class Card:
+    suite = None
+    rank = None
+
+    def __init__(self, suite, rank):
+        self.suite = suite
+        self.rank = rank
+
+    def __str__(self):
+        return 'suite{}, rank{}'.format(self.suite, self.rank)
+
+class Deck:
+
+
