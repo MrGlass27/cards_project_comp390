09e90053ff1054cb4bba50f74036aa10ff248747
matthew <m1glassman@student.bridgew.edu>
2022-12-04 20:53:38 -0500

    created class for rank and suite of the cards.

---
+++ cards.py
@@ -0,0 +1,16 @@
+import random
+from behave import *
+
+
+class Rank:
+    rank = None
+
+    def __init__(self, rank):
+        self.rank = rank
+
+class Suite:
+    suite = None
+
+    def __init__(self, suite):
+        self.suite = suite
+
